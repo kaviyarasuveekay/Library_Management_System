c5599ce8684c078313a74cfe9d23ee4a43698454
Kaviyarasu V <84955527+kaviyarasuveekay@users.noreply.github.com>
2022-11-12 23:09:07 +0530

    Add files via upload

---
+++ librarian/footer.php
@@ -0,0 +1,22 @@
+</div>
+<!--scroll to top-->
+<a href="#" class="scroll-to-top"><i class="fa fa-angle-double-up"></i></a>
+</div>
+</div>
+<!--BASIC scripts-->
+<!-- ========================================================= -->
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="../assets/vendor/jquery/jquery-1.12.3.min.js"></script>
+<script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="../assets/vendor/nano-scroller/nano-scroller.js"></script>
+<!--TEMPLATE scripts-->
+<!-- ========================================================= -->
+<script src="../assets/javascripts/template-script.min.js"></script>
+<script src="../assets/javascripts/template-init.min.js"></script>
+<!-- SECTION script and examples-->
+<!-- ========================================================= -->
+<!--Notification msj-->
+
+</body>
+
+</html>
---
+++ librarian/index.php
@@ -0,0 +1,91 @@
+
+<?php 
+require_once 'header.php'; 
+?>
+<div class="content-header">
+    <!-- leftside content header -->
+    <div class="leftside-content-header">
+        <ul class="breadcrumbs">
+            <li><i class="fa fa-home" aria-hidden="true"></i><a href="#">Dashboard</a></li>
+        </ul>
+    </div>
+</div>    
+<div class="row animated fadeInUp">
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel widgetbox wbox-2 bg-darker-2 color-light" style="background:red">
+            <a href="requestbook.php">
+                <div class="panel-content">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <span class="icon fa fa-book color-lighter-1"></span>
+                        </div>
+
+                        <?php
+                        $sql = "SELECT * FROM `request_book`";
+                        $res = mysqli_query($con,$sql);
+                        $total_req = mysqli_num_rows($res);
+                        ?>
+                        <div class="col-xs-8">
+                            <h4 class="subtitle color-lighter-1">Request Books</h4>
+                            <h1 class="title color-light"><b><?= $total_req?></b></h1>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel widgetbox wbox-2 bg-darker-2 color-light" style="background:red">
+            <a href="students.php">
+                <div class="panel-content">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <span class="icon fa fa-users color-lighter-1"></span>
+                        </div>
+                        <?php
+    $sql = "SELECT * FROM `students`";
+                                $res = mysqli_query($con,$sql);
+                                $total_students = mysqli_num_rows($res);
+                        ?>
+                        <div class="col-xs-8">
+                            <h4 class="subtitle color-lighter-1">Total Student</h4>
+                            <h1 class="title color-light"><b><?= $total_students?></b></h1>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel widgetbox wbox-2 bg-darker-2 color-light" style="background:red">
+            <a href="managebook.php">
+                <div class="panel-content">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <span class="icon fa fa-book color-lighter-1"></span>
+                        </div>
+                        <?php
+    $sql = "SELECT * FROM `books`";
+                                $res = mysqli_query($con,$sql);
+                                $total_books = mysqli_num_rows($res);
+                        ?>
+                        <div class="col-xs-8">
+                            <h4 class="subtitle color-lighter-1">Total Books</h4>
+                            <h1 class="title color-light"><?= $total_books?></h1>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div> 
+     <div class="col-sm-6 col-md-4 col-lg-3">
+    <div class="panel widgetbox wbox-2 bg-darker-2 color-light" style="background:red">
+        <a href="index.php">
+            
+        </a>
+    </div>
+    </div>
+    
+</div>
+
+<?php include('footer.php'); ?>
